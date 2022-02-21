b1fdc5b9ceda1b3e7cc89b926582f6d64048f343
michalovadek <michal.ovadek@gmail.com>
2022-02-21 17:45:08 +0100

    test-fetch

--- tests/testthat/test-fetch.R
+++ tests/testthat/test-fetch.R
@@ -4,9 +4,8 @@ testthat::test_that("fetching data works", {
   
   out <- eurlex::elx_fetch_data(url = "http://publications.europa.eu/resource/celex/32014R0001",
                                 type = "notice",
-                                notice = "branch") |> 
-    as.character()
+                                notice = "branch")
   
-  testthat::expect_gt(nchar(out), 1000)
+  testthat::expect_gt(nchar(as.character(out)), 1000)
   
 })
