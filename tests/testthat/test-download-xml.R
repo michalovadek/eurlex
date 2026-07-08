7bfff5945b63f32bcad021778c001b2a52cc3a5d
Kristian Vepsäläinen <kristian.vepsalainen@proton.me>
2026-07-08 16:18:42 +0200

    test: add unit tests for elx_download_xml

---
+++ tests/testthat/test-download-xml.R
@@ -0,0 +1,67 @@
+testthat::test_that("elx_download_xml downloads object notice", {
+  
+  testthat::skip_on_cran()
+  
+  temploc <- file.path(tempdir(), "elxnotice_object.xml")
+  on.exit(unlink(temploc))
+  
+  result <- eurlex::elx_download_xml(
+    url = "http://publications.europa.eu/resource/celex/32022D0154",
+    file = temploc, 
+    notice = "object"
+  )
+  
+  testthat::expect_true(file.exists(temploc))
+  testthat::expect_gt(file.size(temploc), 1000)
+  
+})
+
+testthat::test_that("elx_download_xml downloads tree notice", {
+  
+  testthat::skip_on_cran()
+  
+  temploc <- file.path(tempdir(), "elxnotice_tree.xml")
+  on.exit(unlink(temploc))
+  
+  result <- eurlex::elx_download_xml(
+    url = "http://publications.europa.eu/resource/celex/32022D0154",
+    file = temploc, 
+    notice = "tree"
+  )
+  
+  testthat::expect_true(file.exists(temploc))
+  testthat::expect_gt(file.size(temploc), 1000)
+  
+})
+
+testthat::test_that("elx_download_xml validates notice argument", {
+  
+  testthat::expect_error(
+    eurlex::elx_download_xml(
+      url = "http://publications.europa.eu/resource/celex/32022D0154",
+      file = tempfile(),
+      notice = "invalid_type"
+    ),
+    "notice type must be correctly specified"
+  )
+  
+})
+
+testthat::test_that("elx_download_xml requires url and notice", {
+  
+  testthat::expect_error(
+    eurlex::elx_download_xml(notice = "object"),
+    "url must be specified"
+  )
+  
+  testthat::expect_error(
+    eurlex::elx_download_xml(url = "http://example.com"),
+    "notice type must be specified"
+  )
+  
+})
+
+
+
+devtools::load_all()
+devtools::test()
