01846acef4dc0c38ce3a840d263704b7313f7a9f
Kristian Vepsäläinen <kristian.vepsalainen@proton.me>
2026-07-08 09:27:34 +0200

    test: add unit tests for national_impl query validity

--- tests/testthat/test-query.R
+++ tests/testthat/test-query.R
@@ -7,3 +7,28 @@ testthat::test_that("queries can be made", {
   testthat::expect_gt(nchar(q), 800)
   
 })
+
+
+testthat::test_that("national_impl query has balanced parentheses", {
+  
+  testthat::skip_on_cran()
+  
+  q <- eurlex::elx_make_query("national_impl")
+  
+  n_open <- lengths(regmatches(q, gregexpr("\\(", q)))
+  n_close <- lengths(regmatches(q, gregexpr("\\)", q)))
+  
+  testthat::expect_equal(n_open, n_close)
+  
+})
+
+testthat::test_that("national_impl query is well-formed", {
+  
+  testthat::skip_on_cran()
+  
+  q <- eurlex::elx_make_query("national_impl")
+  
+  testthat::expect_gt(nchar(q), 800)
+  testthat::expect_true(grepl("MEAS_NATION_IMPL", q))
+  
+})
